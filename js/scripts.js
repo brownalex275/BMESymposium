2c4b59ac07119587e573708810d3c32d8c401b86
brownalex275 <brownalex275@gmail.com>
2018-04-08 16:13:34 -0400

    Added files to make the registration form work

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,27 @@ $('a[href^="#"]').on('click', function (event) {
 		}, 1000);
 	}
 });
+
+function submitForm() {
+	var fname = document.getElementById("fname").value;
+	var email = document.getElementById("email").value;
+	var lname = document.getElementById("lname").value;
+	var org = document.getElementById("org").value;
+	// Returns successful data submission message when the entered information is stored in database.
+	var dataString = 'fname1=' + fname + '&email1=' + email + '&lname1=' + lname + '&org1=' + org;
+	if (fname == '' || email == '' || lname == '' || org == '') {
+		alert("Please Fill All Fields");
+	} else {
+		// AJAX code to submit form.
+		$.ajax({
+			type: "POST",
+			url: "process.php",
+			data: dataString,
+			cache: false,
+			success: function (html) {
+				alert('Thank you for registering!');
+			}
+		});
+	}
+	return false;
+}
---
+++ process.php
@@ -0,0 +1,30 @@
+<?php
+// Fetching Values From URL
+$fname2 = $_POST['fname1'];
+$lname2 = $_POST['lname1'];
+$email2 = $_POST['email1'];
+$org2 = $_POST['org1'];
+$db_host = 'localhost';
+$db_username = 'jbme_admin';
+$db_password = 'jbme123!';
+$db_name = 'jbme_2018participants';
+$connection = mysql_connect( $db_host, $db_username, $db_password) or die(mysql_error("Could not establish connection to $db_name")); // Establishing Connection with Server..
+$db = mysql_select_db($db_name); // Selecting Database
+
+if (isset($_POST['name1'])) {
+
+$query = mysql_query("CREATE TABLE IF NOT EXISTS registrants(
+registrant_id int(10) NOT NULL AUTO_INCREMENT,
+registrant_fname varchar(255) NOT NULL,
+registrant_lname varchar(255) NOT NULL,
+registrant_email varchar(255) NOT NULL,
+registrant_org varchar(255) NOT NULL,
+PRIMARY KEY (registrant_id));
+
+INSERT INTO registrants( registrant_fname, registrant_lname, registrant_email, registrant_org) values ('$fname2', '$lname2','$email2', '$org2'));")//Insert Query
+echo "Form submitted succesfully.";
+}
+
+
+mysql_close($connection); // Connection Closed
+?>
