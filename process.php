76f4963fc848e74f3050d2c9dab5d2979ae8eae7
brownalex275 <brownalex275@gmail.com>
2018-04-09 13:38:56 -0400

    Changed process.php and schedule style

--- process.php
+++ process.php
@@ -8,23 +8,19 @@ $db_host = 'localhost';
 $db_username = 'jbme_admin';
 $db_password = 'jbme123!';
 $db_name = 'jbme_2018participants';
-$connection = mysql_connect( $db_host, $db_username, $db_password) or die(mysql_error("Could not establish connection to $db_name")); // Establishing Connection with Server..
-$db = mysql_select_db($db_name); // Selecting Database
+// Establishing Connection with Server..
+$conn = new mysqli($db_host, $db_username, $db_password,$db_name);
 
-if (isset($_POST['name1'])) {
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+$sql = "INSERT INTO registrants( registrant_fname, registrant_lname, registrant_email, registrant_org) VALUES ('$fname2', '$lname2','$email2', '$org2')"; //Insert Query
 
-$query = mysql_query("CREATE TABLE IF NOT EXISTS registrants(
-registrant_id int(10) NOT NULL AUTO_INCREMENT,
-registrant_fname varchar(255) NOT NULL,
-registrant_lname varchar(255) NOT NULL,
-registrant_email varchar(255) NOT NULL,
-registrant_org varchar(255) NOT NULL,
-PRIMARY KEY (registrant_id));
-
-INSERT INTO registrants( registrant_fname, registrant_lname, registrant_email, registrant_org) values ('$fname2', '$lname2','$email2', '$org2'));")//Insert Query
-
+if ($conn->query($sql) === TRUE) {
+	echo "New record created successfully";
+} else {
+	echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-
-mysql_close($connection); // Connection Closed
+$conn->close();
 ?>
